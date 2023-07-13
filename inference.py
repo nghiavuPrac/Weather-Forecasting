5f5257eacdede3641117d0236c41fc0e4daf6fd2
nghiavuPrac <nghiavutrong1998@gmail.com>
2023-07-14 01:59:28 +0700

    finishing inference.py

--- inference.py
+++ inference.py
@@ -1,75 +1,104 @@
-# import tensorflow as tf
-# from keras.models import Sequential
-# from keras.layers import GRU, Dense
-# from keras.callbacks import EarlyStopping
-import keras
-import matplotlib.pyplot as plt
+import tensorflow as tf
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import GRU, Dense
+# from tensorflow.keras.callbacks import EarlyStopping
 
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from category_encoders import BinaryEncoder
-
 import pandas as pd
 import os
 import numpy as np
 
-path = './DBM301/encoded.csv'
-encoded_df = pd.read_csv(path)
+
+df_path = './DBM301/clean_data.csv'
+df = pd.read_csv(df_path, index_col=[0])
+df['date']=pd.to_datetime(df['date'])
+
+
+encoded_df = df.copy()
+# Binary encode province feature
+encoded_df['copy_province'] = encoded_df['province']
+province_encoder = BinaryEncoder(cols=['copy_province'])
+encoded_df = province_encoder.fit_transform(encoded_df)
+
+binary_province_feature = ['copy_province_0', 'copy_province_1', 'copy_province_2', 'copy_province_3', 'copy_province_4', 'copy_province_5']
+encoded_df[binary_province_feature] = encoded_df[binary_province_feature].astype('int8')
+
+def cap_and_floor_outliers_iqr(df, columns, k=1.5):
+    # Capping and flooring outliers
+    in_outlier=list()
+    for column in columns:
+      Q1 = df[column].quantile(0.25)
+      Q3 = df[column].quantile(0.75)
+      IQR = Q3 - Q1
+
+      lower_bound = Q1 - k * IQR
+      upper_bound = Q3 + k * IQR
+
+      count= len(df[(df[column] > lower_bound) & (df[column]< upper_bound)])
+      in_outlier.append(count)
+
+      df[column] = df[column].clip(lower_bound, upper_bound)
+
+    return df, in_outlier
+
+encoded_df, _ = cap_and_floor_outliers_iqr(encoded_df, ['max', 'min', 'pressure', 'humidi', 'cloud', 'wind'])
+
+# Select feature
+
+provinces = ['province', 'copy_province_0', 'copy_province_1','copy_province_2', 'copy_province_3', 'copy_province_4','copy_province_5']
+normalize_feature = ['max', 'min', 'rain', 'pressure', 'humidi', 'cloud', 'wind']
+num_feature = len(normalize_feature)
+
+
+# Min max scale
+scaled_columns = ['_'.join(['scaled', column]) for column in normalize_feature]
+
+scaler = MinMaxScaler()
+scaled_data = scaler.fit_transform(encoded_df[normalize_feature])
+encoded_df[scaled_columns] = pd.DataFrame(scaled_data, columns=scaled_columns)
+
+selected_df = encoded_df[provinces+scaled_columns]
+
+
+# Parameters
 
 past = 60 # Number of days in the past used to predict
 step = 1 # Distance between days
 distin_feature = 'province'
-# target = scaled_columns + ['province']
-# df_length = len(selected_df)
+target = scaled_columns + ['province']
+df_length = len(selected_df)
 future_length = 5
 ratio = 0.8
-dayth = 0
-
-
-# Select feature
-features = ['max', 'min', 'rain', 'pressure', 'humidi', 'cloud', 'wind', 'province', 'copy_province_0', 'copy_province_1',
-       'copy_province_2', 'copy_province_3', 'copy_province_4', 'copy_province_5']
-normalize_feature = ['max', 'min', 'rain', 'pressure', 'humidi', 'cloud', 'wind']
-num_feature = len(normalize_feature)
-
-selected_df = encoded_df[features]
-
-# Min max scale
-
-scaled_columns = ['_'.join(['scaled', column]) for column in normalize_feature]
-
-scaler = MinMaxScaler()
-scaled_data = scaler.fit_transform(selected_df[normalize_feature])
-selected_df[scaled_columns] = pd.DataFrame(scaled_data, columns=scaled_columns)
-
-selected_df = selected_df.drop(normalize_feature, axis=1)
 
 #load model
 file_path = './DBM301/model2.h5'
-overlap = True
+overlap = False 
 history = []
 
-
-if os.path.exists(file_path) and overlap==True:
-    model = keras.models.load_model(file_path)
+if os.path.exists(file_path) and overlap==False:
+  model = tf.keras.models.load_model(file_path)
 else:
-    print("Loading Fail!")
-    exit()
+  print("Loading Failed!")
+  exit()
+
+model.summary()
 
 
-province_list=[]
+#@title Input fields
+dayth = 0
 
-
-#Input fields
 
 Province = "Bac Lieu" #@param ["Bac Lieu", "Ben Tre", "Bien Hoa", "Buon Me Thuot", "Ca Mau", "Cam Pha", "Cam Ranh", "Can Tho", "Chau Doc", "Da Lat", "Ha Noi", "Hai Duong", "Hai Phong", "Hanoi", "Ho Chi Minh City", "Hoa Binh", "Hong Gai", "Hue", "Long Xuyen", "My Tho", "Nam Dinh", "Nha Trang", "Phan Rang", "Phan Thiet", "Play Cu", "Qui Nhon", "Rach Gia", "Soc Trang", "Tam Ky", "Tan An", "Thai Nguyen", "Thanh Hoa", "Tra Vinh", "Tuy Hoa", "Uong Bi", "Viet Tri", "Vinh", "Vinh Long", "Vung Tau", "Yen Bai"]
-predicted_number_of_days = 2 #@param {type:"integer"}
+predicted_number_of_days = 5 #@param {type:"integer"}
 
 # Get input data
 input = selected_df[selected_df['province']==Province][-past:].drop('province', axis=1).values.tolist().copy()
-index = encoded_df[selected_df['province']==Province][-past:]
+index = df[selected_df['province']==Province][-past:]
 
 province_encoder = BinaryEncoder(cols=['province'])
-province_encoder.fit_transform(encoded_df['province'])
+province_encoder.fit_transform(df['province'])
 encoded_province = province_encoder.transform(pd.DataFrame({'province': [Province]})).values.tolist()[0]
 
 # Predict
@@ -78,32 +107,11 @@ for day in range(predicted_number_of_days):
 
 result = pd.DataFrame(np.array(input), columns=selected_df.columns[1:])
 
+past_date = df['date'][-past:]
+predict_date = [df['date'].iloc[-1] + pd.Timedelta(days=i) for i in range(predicted_number_of_days+1)]
 
-# Rescale prediction
-# s1 = scaler.inverse_transform(result[scaled_columns])
-# result[normalize_feature] = pd.DataFrame(s1, columns=normalize_feature)
-# result = result.drop(scaled_columns, axis=1)
-
-past_date = encoded_df['date'][-past:]
-predict_date = [encoded_df['date'].iloc[-1] + pd.Timedelta(days=i) for i in range(predicted_number_of_days+1)]
-
-print(predict_date[0])
-# print(type(past_date[0]))
 
 rescale_data = np.round(scaler.inverse_transform(result.values[:, -7:]), 2)
-predict_df = pd.DataFrame(rescale_data, columns=normalize_feature, index=predict_date)
-predict_df[-5:]
-print(predict_df)
-
-# plt.figure(figsize=(22, 22))
-# plt.subplots_adjust(wspace=0.4, hspace=0.6)
-
-# for i in range(num_feature):
-#   plt.subplot(num_feature, 1, i+1)
-#   plt.plot(encoded_df['date'][-past:], result.iloc[:past][normalize_feature[i]], 'o-', label='Dữ liệu')
-#   plt.plot(predict_date, result.iloc[-(predicted_number_of_days+1):][normalize_feature[i]], 'o-', color='orange', label='Dự đoán')
-#   plt.title((normalize_feature[i]).upper())
-#   plt.legend()
-
-
-# plt.show()
+predict_df = pd.DataFrame(rescale_data, columns=normalize_feature)
+predict_df.index = list(past_date[:-1])+predict_date
+print(predict_df[-predicted_number_of_days:])
